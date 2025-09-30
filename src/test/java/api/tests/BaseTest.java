c9495793740f01332fc6fc51b08dc9df1d4e07b7
Hindumathi-B <hindumathib25@gmail.com>
2025-09-30 12:43:03 +0530

    Update BaseTest.java
    Added comments

--- src/test/java/api/tests/BaseTest.java
+++ src/test/java/api/tests/BaseTest.java
@@ -15,16 +15,21 @@ import io.restassured.response.Response;
 public class BaseTest {
 	
 	Login data;
+
+	/*
+	since all the methods(expect testDeleteUser) in the class needs username and password , 
+	using a beforeClass annotation from testng to set the data before the methods in the class starts its execution
+	*/
 	
 	@BeforeClass
 	public void setUpData() {
 		data=new Login();
 		
-		data.setUserName("SampleUser8");
-		data.setPassword("$ampleUser@8");
+		data.setUserName("SampleUser10");
+		data.setPassword("$ampleUser@10");
 	}
 	
-	
+	// [Epic, Feature, Story, Description are the annotations supported by allure for detailed reporting.
 	@Epic("EP001")
 	@Feature("User Feature")
 	@Story("User Creation")
@@ -35,8 +40,8 @@ public class BaseTest {
 	public void testUser(ITestContext context) {
 		Response response=AccountEndpoints.user(data);
 		response.then().log().all();
-		String user_id=response.jsonPath().getString("userID");
-		context.setAttribute("user_id", user_id);
+		String user_id=response.jsonPath().getString("userID");   
+		context.setAttribute("user_id", user_id);            // globalising the user_id value 
 		
 	}
 	
@@ -52,7 +57,7 @@ public class BaseTest {
 		Response response=AccountEndpoints.generateToken(data);
 		response.then().log().all();
 		String token=response.jsonPath().getString("token");
-				context.setAttribute("user_token", token);
+				context.setAttribute("user_token", token);       // Globalising the token value
 				
 			System.out.println("Generated Token: "+ context.getAttribute("user_token"));		
 	}
@@ -79,11 +84,14 @@ public class BaseTest {
 	
 	@Description("Deleting the created user by user id")
 	
-	
+	/*We can't delete the user( by id or not) if the user is not created in first place
+	   since  the method "testDeleteUser" is dependent on the method "testUser",
+	   so used a testng annotation 'dependsOnMethods' testng annotation - if the testUser fails this method will not run
+	*/
 	@Test(priority=4, dependsOnMethods={"testUser"})
 	public void testDeleteUser(ITestContext context) {
-		String user_id=(String) context.getAttribute("user_id");
-		Response response=AccountEndpoints.deleteUserbyID(user_id);
+		String user_id=(String) context.getAttribute("user_id");      //fetching the value of user_id from 'testUser' method and storing in a variable
+		Response response=AccountEndpoints.deleteUserbyID(user_id);    // passing user_id as path parameter.
 		response.then().log().all();
 	}
 	
@@ -91,3 +99,4 @@ public class BaseTest {
 	
 
 }
+
